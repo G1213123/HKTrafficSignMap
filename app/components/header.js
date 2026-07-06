6daf80f560d762c8a171f304511dffc2e479746f
G1213123 <ngwingfung2136@yahoo.com.hk>
2026-07-06 21:18:25 +0800

    update session cookies

--- app/components/header.js
+++ app/components/header.js
@@ -1,24 +1,19 @@
 "use client";
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import {
-    signInWithGoogle,
-    signOut,
-    onIdTokenChanged,
-} from '../lib/firebase/auth';
+import { signInWithGoogle, signOut, onAuthStateChanged, onIdTokenChanged } from '../../lib/firebase/auth';
 import  {setCookie, deleteCookie} from 'cookies-next';
 
-function useUserSession(initialUser) {
-    useEffect(() => {
-        return onIdTokenChanged(async (user) => {
-            if (user) {
-                const idToken = await user.getIdToken();
-                await setCookie('__session', idToken);
-            } else {
-                await deleteCookie('__session');
-            }
-            if (initialUser?.uid === user?.uid) return;
-            window.location.reload();
-        });
-    }, [initialUser]);
+export function useUserSession() {
+  useEffect(() => {
+    return onAuthStateChanged(async (user) => {
+      if (user) {
+        const idToken = await user.getIdToken();
+        await setCookie('__session', idToken);
+      } else {
+        await deleteCookie('__session');
+      }
+      // Removed window.location.reload() to prevent infinite refresh loops
+    });
+  }, []);
 }
